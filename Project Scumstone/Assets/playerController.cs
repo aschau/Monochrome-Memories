5b2f326faddb1a5594a303bb8a4816258b379b0c
Andrew Steven Chau <aschau@uci.edu>
2016-10-02 12:32:33 -0700

    Got split screen working
    -Tiffany, Andrew

---
+++ Project Scumstone/Assets/playerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+    public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(new Vector2(speed * Time.deltaTime, 0f));
+        }
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(new Vector2(-speed * Time.deltaTime, 0f));
+        }
+
+	}
+}
