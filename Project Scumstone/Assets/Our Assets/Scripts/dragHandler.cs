9ed1b9b7c00d2c242e9a96d38d57545042921565
Andrew Steven Chau <aschau@uci.edu>
2016-10-06 16:03:41 -0700

    created dragHandler.cs for the UI dragging system

---
+++ Project Scumstone/Assets/Our Assets/Scripts/dragHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragHandler : MonoBehaviour {
+    private float offsetX;
+    private float offsetY;
+    private Vector3 origin;
+	// Use this for initialization
+	void Start () {
+        origin = this.transform.position;   
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void beginDrag()
+    {
+        offsetX = this.transform.position.x - Input.mousePosition.x;
+        offsetY = this.transform.position.y - Input.mousePosition.y;
+    }
+
+    public void onDrag()
+    {
+        this.transform.position = new Vector3(Input.mousePosition.x + offsetX, Input.mousePosition.y + offsetY);
+    }
+
+    public void endDrag()
+    {
+        this.transform.position = origin;
+    }
+}
