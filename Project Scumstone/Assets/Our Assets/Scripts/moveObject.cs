ef3b66362b4a4c8f47a5ca4534249b6baad1bbae
Andrew Steven Chau <aschau@uci.edu>
2016-10-06 13:14:57 -0700

    added Prefabs/p1PushableBlock.prefab and moveObject.cs

---
+++ Project Scumstone/Assets/Our Assets/Scripts/moveObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveObject : MonoBehaviour {
+    public bool activated = false;
+
+	// Use this for initialization
+	void Start () {
+	    if (!activated)
+        {
+            this.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (activated)
+        {
+            this.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+	}
+}
