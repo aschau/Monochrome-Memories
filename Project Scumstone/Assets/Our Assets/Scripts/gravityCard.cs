2449597dcfb938a0e91439e08638a8fa4f33adfc
Andrew Steven Chau <aschau24@gmail.com>
2016-10-18 12:06:15 -0700

    Refactored how card scripts work. Now inherits from card.cs. added fallBlock.prefab, floatBlock.prefab. Updated Canvas.prefab

---
+++ Project Scumstone/Assets/Our Assets/Scripts/gravityCard.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravityCard : card {
+    private RaycastHit2D hit;
+
+    public override void onMouseUp()
+    {
+        hit = checkHit(camera1);
+        if (hit)
+        {
+            if (hit.transform.CompareTag("fallBlock"))
+            {
+                Debug.Log("ACTIVATED FALL BLOCK");
+                hit.transform.GetComponent<fallObject>().activated = true;
+            }
+
+        }
+
+        else
+        {
+            hit = checkHit(camera2);
+            if (hit)
+            {
+                if (hit.transform.CompareTag("floatBlock"))
+                {
+                    Debug.Log("ACTIVATED Float BLOCK");
+                    hit.transform.GetComponent<floatObject>().activated = true;
+                }
+
+            }
+        }
+
+        base.onMouseUp();
+    }
+}
