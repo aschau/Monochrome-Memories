d1af0f66b2b7943c383518feb97d9c9ec817bed5
Tiffany Lambo <lambocreates@gmail.com>
2016-11-01 16:58:27 -0700

    Level 2 adjustments

--- Project Scumstone/Assets/Our Assets/Scripts/fallObject.cs
+++ Project Scumstone/Assets/Our Assets/Scripts/fallObject.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class fallObject : MonoBehaviour {
     public bool activated = false;
     public float speed = 5f;
+    private bool falling = false;
     
     // Use this for initialization
     public  void Start()
@@ -12,6 +13,7 @@ public class fallObject : MonoBehaviour {
         {
             this.GetComponent<Rigidbody2D>().isKinematic = true;
         }
+        //this.transform.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
     }
     void FixedUpdate()
     {
@@ -19,6 +21,15 @@ public class fallObject : MonoBehaviour {
         {
             this.GetComponent<Rigidbody2D>().isKinematic = false;
             this.transform.Translate(new Vector2(0, -this.speed * Time.deltaTime));
+            if (this.GetComponent<moveObject>())
+            {
+                this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+            }
         }
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        this.activated = false;
     }
 }
--- Project Scumstone/Assets/Our Assets/Scripts/floatObject.cs
+++ Project Scumstone/Assets/Our Assets/Scripts/floatObject.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class floatObject : MonoBehaviour {
     public bool activated = false;
     public float speed = 5f;
+    public float variance = 0f;
+    private float originalY = 0f;
 
     // Use this for initialization
     public void Start()
     {
+        this.originalY = this.transform.position.y;
         if (!this.activated)
         {
             this.GetComponent<Rigidbody2D>().isKinematic = true;
@@ -19,7 +22,18 @@ public class floatObject : MonoBehaviour {
         if (this.activated)
         {
             this.GetComponent<Rigidbody2D>().isKinematic = false;
-            this.transform.Translate(new Vector2(0, this.speed * Time.deltaTime));
+            if (this.transform.position.y <= (this.originalY + this.variance))
+            {
+                this.transform.Translate(new Vector2(0, this.speed * Time.deltaTime));
+            }
+            else
+            {
+                this.GetComponent<Rigidbody2D>().isKinematic = true;
+            }
         }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        this.activated = false;
     }
 }
