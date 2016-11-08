5e7265c59d55acf3a2ee121d3b9766be39d37b1c
Tiffany Lambo <lambocreates@gmail.com>
2016-11-07 23:51:22 -0800

    finished demo top level 2

---
+++ Project Scumstone/Assets/Our Assets/Scripts/droppingItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class droppingItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            Debug.Log("Collided");
+            this.transform.GetComponent<Rigidbody2D>().isKinematic = false;
+        }
+    }
+}
