bf7b97501e9ef56698efa715357b7c49dc56a1f2
Tiffany Lambo <lambocreates@gmail.com>
2016-11-14 18:12:24 -0800

    added level 3

---
+++ Project Scumstone/Assets/Our Assets/Scripts/makeObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makeObject : MonoBehaviour {
+    public bool activated = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (activated)
+        {
+            this.enabled = false;
+        }
+	
+	}
+}
