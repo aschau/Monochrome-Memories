0d5cff0f414514a11d6926b66e7983f279b4f398
Andrew Steven Chau <aschau24@gmail.com>
2016-11-21 04:10:35 -0800

    Almost done with walking animation. One kink when pressing both directions. Separated part of playerController.cs into playerMovement.cs. Reorganized script file structure. also slightly modified Canvas.prefab.

--- Project Scumstone/Assets/Our Assets/Scripts/playerController.cs
+++ Project Scumstone/Assets/Our Assets/Scripts/playerController.cs
@@ -1,129 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class playerController : MonoBehaviour {
-    public float moveSpeed = 3f;
-    public float p1JumpSpeed = 200f, p2JumpSpeed = 200f;
-    public float originalMoveSpeed;
-    public float originalJumpSpeed;
-
-    private int playerNumber = 1;
     private GameObject player1, player2;
-    private GameObject topCover, bottomCover;
+    private Image topCover, bottomCover;
 
     void Awake()
     {
         this.player1 = GameObject.Find("Player");
         this.player2 = GameObject.Find("Player 2");
-        this.topCover = GameObject.Find("topImage");
-        this.bottomCover = GameObject.Find("bottomImage");
+        this.topCover = GameObject.Find("topImage").GetComponent<Image>();
+        this.bottomCover = GameObject.Find("bottomImage").GetComponent<Image>();
     }
 
 	// Use this for initialization
 	void Start () {
-        this.topCover.SetActive(false);
-        this.playerNumber = 1;
-        this.originalMoveSpeed = this.moveSpeed;
-        this.originalJumpSpeed = this.p1JumpSpeed;
+        this.bottomCover.enabled = true;
 	}
 
     void Update()
     {
         if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.E)) && (this.player1.activeSelf && this.player2.activeSelf))
         {
-            if (playerNumber == 1)
+            if (playerMovement.player == "Player")
             {
-                playerNumber = 2;
-                bottomCover.SetActive(false);
-                topCover.SetActive(true);
+                playerMovement.player = "Player 2";
+                bottomCover.enabled = false;
+                topCover.enabled = true;
             }
 
-            else if (playerNumber == 2)
+            else if (playerMovement.player == "Player 2")
             {
-                playerNumber = 1;
-                topCover.SetActive(false);
-                bottomCover.SetActive(true);
+                playerMovement.player = "Player";
+                topCover.enabled = false;
+                bottomCover.enabled = true;
             }
         }
 
         else if (!this.player1.activeSelf && this.player2.activeSelf)
         {
-            playerNumber = 2;
-            bottomCover.SetActive(false);
-            topCover.SetActive(true);
+            playerMovement.player = "Player 2";
+            bottomCover.enabled = false;
+            topCover.enabled = true;
         }
 
         else if (this.player1.activeSelf && !this.player2.activeSelf)
         {
-            playerNumber = 1;
-            topCover.SetActive(false);
-            bottomCover.SetActive(true);
+            playerMovement.player = "Player";
+            topCover.enabled = false;
+            bottomCover.enabled = true;
         }
     }   
-
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (Input.GetKey(KeyCode.D))
-        {
-            if (playerNumber == 1)
-            {
-                this.player1.transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0f));
-                //this.player1.GetComponent<SpriteRenderer>().enabled = false;
-                //this.player1.transform.FindChild("Armature").gameObject.SetActive(true);
-                //this.player1.transform.FindChild("Armature").localScale = new Vector3(Mathf.Abs(this.player1.transform.localScale.x), this.player1.transform.localScale.y, this.player1.transform.localScale.z);
-                //this.player1.GetComponent<SpriteRenderer>().flipX = false;
-            }
-
-            else if (playerNumber == 2)
-            {
-                this.player2.transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0f));
-            }
-        }
-
-        //else if (Input.GetKeyUp(KeyCode.D))
-        //{
-        //    this.player1.transform.FindChild("Armature").gameObject.SetActive(false);
-        //    this.player1.GetComponent<SpriteRenderer>().enabled = true;
-        //}
-
-        else if (Input.GetKey(KeyCode.A))
-        {
-            if (playerNumber == 1)
-            {
-                this.player1.transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0f));
-                //this.player1.GetComponent<SpriteRenderer>().enabled = false;
-                //this.player1.transform.FindChild("Armature").gameObject.SetActive(true);
-                //this.player1.transform.FindChild("Armature").localScale = new Vector3(-Mathf.Abs(this.player1.transform.localScale.x), this.player1.transform.localScale.y, this.player1.transform.localScale.z);
-                //this.player1.GetComponent<SpriteRenderer>().flipX = true;
-            }
-
-            else if (playerNumber == 2)
-            {
-                this.player2.transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0f));
-            }
-        }
-
-        //else if (Input.GetKeyUp(KeyCode.A))
-        //{
-        //    this.player1.transform.FindChild("Armature").gameObject.SetActive(false);
-        //    this.player1.GetComponent<SpriteRenderer>().enabled = true;
-        //}
-
-        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
-        {
-
-            if (this.player1.transform.Find("groundDetect").GetComponent<groundCheck>().onGround && playerNumber == 1)
-            {
-                this.player1.transform.Find("groundDetect").GetComponent<groundCheck>().onGround = false;
-                this.player1.GetComponent<Rigidbody2D>().AddForce(Vector2.up * p1JumpSpeed);
-            }
-
-            else if (this.player2.transform.Find("groundDetect").GetComponent<groundCheck>().onGround && playerNumber == 2)
-            {
-                this.player2.transform.Find("groundDetect").GetComponent<groundCheck>().onGround = false;
-                this.player2.GetComponent<Rigidbody2D>().AddForce(Vector2.up * p2JumpSpeed);
-            }
-
-        }
-	}
 }
---
+++ Project Scumstone/Assets/Our Assets/Scripts/playerMovement.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections;
+using DragonBones;
+
+public enum Direction
+{
+    Right,
+    Left,
+    None
+}
+
+public class playerMovement : MonoBehaviour {
+    public static string player = "Player";
+
+    public float moveSpeed = 3f, jumpSpeed = 200f, originalMoveSpeed, originalJumpSpeed;
+    private bool walkingLeft, walkingRight, idle, idleReady = false;
+    private Direction lastDirection = Direction.None, currentDirection = Direction.None;
+    private DragonBones.Animation anim;
+
+	// Use this for initialization
+	void Start () {
+        this.originalMoveSpeed = this.moveSpeed;
+        this.originalJumpSpeed = this.jumpSpeed;
+        this.anim = this.GetComponent<UnityArmatureComponent>().animation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.name == player)
+        {
+            //if (Input.GetKeyDown(KeyCode.D))
+            //{
+            //    //this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+            //    this.animation.FadeIn("Walking", 0.3f);
+            //    //this.walkingRight = true;
+            //}
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                this.transform.localScale = new Vector3(Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+                this.walkingRight = true;
+                this.currentDirection = Direction.Right;
+
+                if (this.currentDirection != this.lastDirection)
+                {
+                    this.anim.FadeIn("Walking", 0.3f);
+                }
+            }
+
+            else if (Input.GetKeyUp(KeyCode.D))
+            {
+                this.walkingRight = false;
+            }
+
+            //if (Input.GetKeyDown(KeyCode.A))
+            //{
+            //    //this.transform.localScale = new Vector3(-Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+            //    this.animation.FadeIn("Walking", 0.3f);
+            //    //this.walkingLeft = true;
+            //}
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                this.transform.localScale = new Vector3(-Mathf.Abs(this.transform.localScale.x), this.transform.localScale.y, this.transform.localScale.z);
+                this.walkingLeft = true;
+                this.currentDirection = Direction.Left;
+                if (this.currentDirection != this.lastDirection)
+                {
+                    this.anim.FadeIn("Walking", 0.3f);
+                }
+            }
+            else if (Input.GetKeyUp(KeyCode.A))
+            {
+                this.walkingLeft = false;
+            }
+
+            Debug.Log(this.currentDirection);
+
+            if (this.walkingLeft ^ this.walkingRight)
+            {
+                this.idle = false;
+                this.idleReady = true;
+            }
+
+            else
+            {
+                this.idle = true;
+            }
+
+            if (this.idle && this.idleReady)
+            {
+                this.anim.FadeIn("Idle", 0.5f);
+                this.idleReady = false;
+            }
+
+            this.lastDirection = this.currentDirection;
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (this.name == player && this.walkingRight && !this.walkingLeft)
+        {
+            if (this.transform.localScale.x < 0)
+            {
+                this.transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0f));
+            }
+
+            else
+            {
+                this.transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0f));
+            }
+        }
+
+        else if (this.name == player && this.walkingLeft && !this.walkingRight)
+        {
+            if (this.transform.localScale.x < 0)
+            {
+                this.transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0f));
+            }
+
+            else
+            {
+                this.transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0f));
+            }
+
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.Space))
+        {
+
+            if (this.transform.Find("groundDetect").GetComponent<groundCheck>().onGround && player == this.name)
+            {
+                this.transform.Find("groundDetect").GetComponent<groundCheck>().onGround = false;
+                this.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed);
+            }
+
+        }
+    }
+}
