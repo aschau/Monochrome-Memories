3efabe2e8a8797a5a9f64909d102a00b65c30a23
Andrew Steven Chau <aschau24@gmail.com>
2016-11-28 18:53:19 -0800

    started testing main menu. commit because laptop is dying.

---
+++ Project Scumstone/Assets/Our Assets/Scripts/UI/exitGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class exitGame : mainMenuButton {
+
+    public override void onClick()
+    {
+        base.onClick();
+        Application.Quit();
+    }
+}
---
+++ Project Scumstone/Assets/Our Assets/Scripts/UI/mainMenuButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class mainMenuButton : MonoBehaviour {
+
+	// Use this for initialization
+	public virtual void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+	
+	}
+
+    public virtual void onClick()
+    {
+
+    }
+
+    public virtual void onMouseEnter()
+    {
+        this.transform.FindChild("Text").gameObject.SetActive(true);
+    }
+
+    public virtual void onMouseExit()
+    {
+        this.transform.FindChild("Text").gameObject.SetActive(false);
+    }
+}
