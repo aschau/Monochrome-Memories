07d5f9e28209c9cbde9e3710545e5082d31d34bc
Limey <jalimeylimey@gmail.com>
2017-01-15 20:25:51 -0800

    Added different audio for White and Black world and implemented it.

---
+++ Project Scumstone/Assets/Our Assets/Scripts/SwitchAudio.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchAudio : MonoBehaviour {
+   
+    public AudioSource _AudioSource1;
+    public AudioSource _AudioSource2;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _AudioSource1.mute = false;
+        _AudioSource2.mute = true; 
+        _AudioSource1.Play();
+        _AudioSource2.Play(); 
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+            if (_AudioSource1.mute == false)
+            {
+
+                _AudioSource1.mute = true; 
+
+                _AudioSource2.mute = false;
+
+            }
+
+            else if (_AudioSource2.mute == false)
+            {
+
+                _AudioSource2.mute = true;
+
+                _AudioSource1.mute = false;
+
+            }
+
+        }
+	}
+}
