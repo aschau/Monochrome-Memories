55eac5011fdec5d6de993f3d2fe29a18851abd99
Jalimey <jalimeylimey@gmail.com>
2017-02-11 18:10:47 -0800

    Added music for Bargaining level

---
+++ Project Scumstone/Assets/Our Assets/Scripts/MuteAudio.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuteAudio : MonoBehaviour {
+
+AudioSource audio;
+    
+    void Start() 
+    {
+    	audio = GetComponent<AudioSource>();
+    }
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E))
+            if (audio.mute)
+                audio.mute = false;
+            else
+                audio.mute = true;
+        
+    }
+}
