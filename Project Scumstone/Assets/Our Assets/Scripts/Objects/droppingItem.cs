15a48bc8c50bc5f2f82da597baa68a931aa76bb2
Tiffany Lambo <lambocreates@gmail.com>
2017-03-15 05:49:31 -0700

    dropping branch works correctly. It rotates to indicate it's a failing branch. There is delay after stepping on it. There is delay for it to respawn.

--- Project Scumstone/Assets/Our Assets/Scripts/Objects/droppingItem.cs
+++ Project Scumstone/Assets/Our Assets/Scripts/Objects/droppingItem.cs
@@ -3,35 +3,90 @@ using System.Collections;
 
 public class droppingItem : MonoBehaviour {
     private Vector2 originalPosition;
-    public AudioSource breakingSound;
-    private int count;
+    public AudioSource breakingSound, brokenSound;
+    public float respawnDelay = 1.0f;
+    public GameObject player;
+    public int count;
+    private bool resetting, touched;
+    public float maxRotation = 5f;
+    //public float amplitude = 5f;
+    private float originalZ;
+    public float speed = 10;
 
 	// Use this for initialization
 
     void Start () {
         this.originalPosition = this.transform.position;
         this.transform.GetComponent<Rigidbody2D>().isKinematic = true;
-        this.count = 0;
+        this.resetting = false;
+        this.originalZ = this.transform.rotation.z;
         //if (this.transform.childCount >= 1)
             //this.transform.GetComponentInChildren<ParticleSystem>().Stop();
 	}
+
+    IEnumerator respawn() //delays the respawning of the branch to original position
+    {
+        yield return new WaitForSeconds(respawnDelay);
+        this.resetting = false;
+        this.transform.GetComponent<Rigidbody2D>().isKinematic = true;
+        this.transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        this.touched = false;
+        this.transform.position = this.originalPosition;
+        yield return 0; 
+    }
+
+    IEnumerator shaking()
+    {
+        this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        yield return new WaitForSeconds(30f);
+        yield return 0;
+        
+    }
 	
 	// Update is called once per frame
     void Update()
     {
-        if (count >= 15)
+        if (!this.touched) //When the player has not interacted with branch it should move
         {
-            this.transform.position = this.originalPosition;
-            this.GetComponent<Rigidbody2D>().isKinematic = true;
-            this.count = -1;
-        }
-        else
-        {
-            if (this.count > -1)
+            this.transform.rotation = Quaternion.Euler(0f, 0f, maxRotation * Mathf.Sin(Time.time * speed));
+            if (Mathf.RoundToInt(Time.fixedTime)%2 == 0)
             {
-                this.count += 1;
+                StartCoroutine(shaking());
             }
         }
+        if (!resetting)
+        {
+            if (player != null)
+            {
+                if (player.name == "Player 2")
+                {
+
+                    GameObject ground = GameObject.Find("Black Platform");
+                    if (this.transform.position.y <= ground.transform.position.y)
+                    {
+
+                        brokenSound.Play();
+                        this.transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                        //Debug.Log("It should be kinematic");
+                        this.resetting = true;
+                        StartCoroutine(respawn());
+                    }
+                }
+                else if (player.name == "Player")
+                {
+                    GameObject ground = GameObject.Find("White Platform");
+                    if (this.transform.position.y <= ground.transform.position.y)
+                    {
+                        brokenSound.Play();
+                        this.transform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+                        this.resetting = true;
+                        StartCoroutine(respawn());
+
+                    }
+                }
+            }
+        }
+       
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -41,13 +96,19 @@ public class droppingItem : MonoBehaviour {
         {
             if (other.transform.position.y > this.transform.position.y)
             {
-                breakingSound.Play(); 
-                this.transform.GetComponent<Rigidbody2D>().isKinematic = false;
+                this.touched = true;
+                StartCoroutine(fall(other.transform.gameObject));
             }
         }
-        else
-        {
-            this.count = 0;
-        }
+    }
+
+    IEnumerator fall(GameObject collidedObject) //slight delay before branch falls
+    {
+        yield return new WaitForSeconds(0.2f);
+        this.player = collidedObject.transform.gameObject;
+        breakingSound.Play();
+        this.transform.GetComponent<Rigidbody2D>().isKinematic = false;
+        yield return 0;
     }
 }
+
