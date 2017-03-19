ba7d73d8690522072eef5aad37262c22456db74f
Andrew Steven Chau <aschau24@gmail.com>
2017-03-18 17:50:10 -0700

    quick fix to pullObject

--- Project Scumstone/Assets/Our Assets/Scripts/Objects/pullObject.cs
+++ Project Scumstone/Assets/Our Assets/Scripts/Objects/pullObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pushObject : baseObject
+public class pullObject : baseObject
 {
     private bool moved;
     public float speed = 5f;
