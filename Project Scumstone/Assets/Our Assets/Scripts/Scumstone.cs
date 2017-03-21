7cd4e061487198c231a62dacd0a73ca5e5cedc40
Limey <jalimeylimey@gmail.com>
2017-03-20 23:31:11 -0700

    - LOGO WORKS NOW ToT

---
+++ Project Scumstone/Assets/Our Assets/Scripts/Scumstone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scumstone : MonoBehaviour
+{
+    public AudioSource slime;
+    // Use this for initialization
+    void Start()
+    {
+        slime.Play();
+        StartCoroutine(LateCall());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator LateCall()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("mainMenu");
+    }
+}
